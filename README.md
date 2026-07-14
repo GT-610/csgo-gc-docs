ef5075a5fec9097f41512873e21968370eb8d26a
GT610 <myddz1005@163.com>
2026-07-14 13:12:33 +0800

    Update README and site src link

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# csgo-gc.github.io
-Website source code of CSGO GC docs
+# CSGO-GC Docs
+Website source code of [CSGO GC docs](https://csgo-gc.gt610.dpdns.org).
+
+Built with [VitePress](https://vitepress.dev/).
+
+Project: https://github.com/GT-610/csgo-gc
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,4 +1,4 @@
-import DefaultTheme from 'vitepress/theme'
+﻿import DefaultTheme from 'vitepress/theme'
 import { h } from 'vue'
 import { useData } from 'vitepress'
 import './custom.css'
@@ -8,19 +8,27 @@ const VercountStats = {
     const { lang } = useData()
     const isChinese = () => lang.value === 'zh-CN'
 
-    return () => h('div', { class: 'vercount-stats', role: 'contentinfo' }, [
-      h('span', null, [
-        isChinese() ? '\u672c\u7ad9\u603b\u8bbf\u95ee\u91cf ' : 'Site PV ',
-        h('span', { id: 'vercount_value_site_pv' }, 'Loading')
+    return () => h('div', { class: 'site-footer-meta', role: 'contentinfo' }, [
+      h('div', { class: 'vercount-stats' }, [
+        h('span', null, [
+          isChinese() ? '\u672c\u7ad9\u603b\u8bbf\u95ee\u91cf ' : 'Site PV ',
+          h('span', { id: 'vercount_value_site_pv' }, 'Loading')
+        ]),
+        h('span', null, [
+          isChinese() ? '\u672c\u7ad9\u8bbf\u5ba2\u6570 ' : 'Site UV ',
+          h('span', { id: 'vercount_value_site_uv' }, 'Loading')
+        ]),
+        h('span', null, [
+          isChinese() ? '\u672c\u9875\u8bbf\u95ee\u91cf ' : 'Page PV ',
+          h('span', { id: 'vercount_value_page_pv' }, 'Loading')
+        ])
       ]),
-      h('span', null, [
-        isChinese() ? '\u672c\u7ad9\u8bbf\u5ba2\u6570 ' : 'Site UV ',
-        h('span', { id: 'vercount_value_site_uv' }, 'Loading')
-      ]),
-      h('span', null, [
-        isChinese() ? '\u672c\u9875\u8bbf\u95ee\u91cf ' : 'Page PV ',
-        h('span', { id: 'vercount_value_page_pv' }, 'Loading')
-      ])
+      h('a', {
+        class: 'source-code-link',
+        href: 'https://github.com/GT-610/csgo-gc-docs',
+        target: '_blank',
+        rel: 'noreferrer'
+      }, isChinese() ? '\u7ad9\u70b9\u6e90\u4ee3\u7801' : 'Site source code')
     ])
   }
 }
